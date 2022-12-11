37e6f552bec4077874cd2e846a7026592630b975
YusukeHashimotoPhD <yhashimoto.research@gmail.com>
2022-12-12 08:51:49 +0900

    Update _12._原因別年内累積死亡数の表示.py

--- pages/_12._原因別年内累積死亡数の表示.py
+++ pages/_12._原因別年内累積死亡数の表示.py
@@ -19,7 +19,7 @@ def calc_weeks_from_new_year(date):
     ny = date.replace(month=1, day=1)
     return int((date - ny).days / 7)
 
-st.title('原因別死亡数のデータ分析')
+st.title('原因別の年内累積死亡数')
 
 st.write('Data source: https://exdeaths-japan.org/#interpretation')
 
