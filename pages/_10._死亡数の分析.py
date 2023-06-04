0ac27e0c96bb7676393ff26fd14e261d2d8da63b
Yusuke Hashimoto <98290434+DrYusukeHashimoto@users.noreply.github.com>
2023-06-04 17:08:11 +0900

    Data is renewed.

--- pages/_10._死亡数の分析.py
+++ pages/_10._死亡数の分析.py
@@ -7,33 +7,34 @@ import os
 st.set_page_config(layout="wide")
 
 st.title("死亡数の分析")
-st.write('データの出展：https://dashboard.e-stat.go.jp/timeSeriesResult?indicatorCode=0203020000000010000')
+st.write('データの出展：https://dashboard.e-stat.go.jp/')
 
 folder_path = './data/'
 file_path = folder_path + 'TimeSeriesResult_20221030155227851.csv'
-#file_path = folder_path + 'TimeSeriesResult_20221211115228753.csv'
-file_path = folder_path + 'TimeSeriesResult_20221211120728541.csv'
+# file_path = folder_path + 'TimeSeriesResult_20221211115228753.csv'
+file_path = folder_path + 'TimeSeriesResult_20230604170613620.csv'
 
 df = pd.read_csv(file_path)
 
 df['年月'] = pd.to_datetime(df['時点'], format='%Y年%m月')
 df['年'] = df['年月'].dt.year
 df['月'] = df['年月'].dt.month
-df_A = pd.crosstab(index = df['年'], columns = df['月'], values = df['死亡数【人】'], aggfunc = np.mean)
+df_A = pd.crosstab(index=df['年'], columns=df['月'], values=df['死亡数【人】'],
+                   aggfunc=np.mean)
 
 fig = px.imshow(df_A.T,
                 title='月毎死亡数の年変化',
-                width = 800,
+                width=800,
                 aspect='auto',
-               )
+                )
 st.plotly_chart(fig, use_container_width=True)
 
 fig = px.imshow(-df_A.diff().T,
                 title='月毎死亡数の対前年差',
-                width = 800,
+                width=800,
                 aspect='auto',
                 color_continuous_scale=px.colors.sequential.RdBu
-               )
+                )
 st.plotly_chart(fig, use_container_width=True)
 
 fig = px.line(df_A,
